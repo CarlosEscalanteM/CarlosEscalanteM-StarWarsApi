95bca0acafe294353a34a28a48635c2ec9d46995
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-03-27 03:47:08 +0000

    Star Wars Project

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p> Made with the help of The Force by Carlos Escalante - 2024 </p>
+		<p> Made with the help of The Force by Carlos Escalante - (2024) </p>
 	</footer>
 );
